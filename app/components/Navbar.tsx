4a908b3cc66c816fbcdd1b1dde800fd9f79eb937
EricPudney <ericpudney@gmail.com>
2025-01-31 21:34:53 +0100

    added empty login/about me pages

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { HomeIcon, ListBulletIcon, PlusCircleIcon } from '@heroicons/react/24/outline'
+import { HomeIcon, ListBulletIcon, PlusCircleIcon, QuestionMarkCircleIcon, LockOpenIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link';
 
 
@@ -9,25 +9,42 @@ export default function Navbar() {
 return(
     <nav className="flex items-center justify-around p-4 bg-orange-700 text-white">
     <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+
     <div>
     <Link href='/home' className="flex items-center">
       <HomeIcon className="h-6 w-6" />
-        <p className='ml-2'>Home</p>
+        <p className='hidden sm:inline-block ml-2 text-sm sm:text-base lg:text-lg'>Home</p>
     </Link>
     </div>
     <div>
     <Link href='/home/booklist' className="flex items-center">
       <ListBulletIcon className="h-6 w-6" />
-        <p className='ml-2'>Booklist</p>
+        <p className='hidden sm:inline-block ml-2 text-sm sm:text-base lg:text-lg'>Booklist</p>
     </Link>
     </div>
     <div>
     <Link href='/home/add' className="flex items-center">
       <PlusCircleIcon className="h-6 w-6" />
-        <p className='ml-2'>Add book</p>
+        <p className='hidden sm:inline-block ml-2 text-sm sm:text-base lg:text-lg'>Add book</p>
+    </Link>
+    </div>
+    <div>
+    <Link href='/home/about' className="flex items-center">
+      <QuestionMarkCircleIcon className="h-6 w-6" />
+        <p className='hidden sm:inline-block ml-2 text-sm sm:text-base lg:text-lg'>About me</p>
     </Link>
     </div>
     <div></div>
+    <div></div>
+    <div></div>
+
+    <Link href='/home/login' className="flex items-center">
+      <LockOpenIcon className="h-6 w-6" />
+        <p className='hidden sm:inline-block ml-2 text-sm sm:text-base lg:text-lg'>Login</p>
+    </Link>
     </nav>
 )
 }
---
+++ app/home/about/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <>
+            <h2 className='mb-4 text-xl md:text-2xl mt-16'>About me</h2>
+        </>        
+)
+}
