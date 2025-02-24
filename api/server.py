1d307800b3233dbfbdf2949ac4e4ae78bb487d8f
EricPudney <ericpudney@gmail.com>
2025-02-24 20:58:59 +0100

    /api/chat -> /chat

--- api/server.py
+++ api/server.py
@@ -18,7 +18,7 @@ class ChatRequest(BaseModel):
     message: str
 
 # seems stupid to divide this up but this has to return a ChatCompletion object which the FE doesn't like - temp fix
-@app.post("/api/chat")
+@app.post("/chat")
 async def chat(request: ChatRequest):
     try:
         response = await getResponse(request)
